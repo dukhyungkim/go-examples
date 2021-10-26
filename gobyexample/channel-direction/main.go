fcb67e8e0e162d13b0e86cd269b97d9993dc9763
DukHyung Kim <idealismboy@gmail.com>
2021-10-26 23:42:16 +0900

    channel direction

---
+++ gobyexample/channel-direction/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// deadlock!
+	//pings := make(chan string)
+	//pongs := make(chan string)
+
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+
+	ping(pings, "passed message")
+	pong(pings, pongs)
+
+	fmt.Println(<-pongs)
+}
+
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
