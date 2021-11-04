8c74726996bf799722c29cb76768990cb76fe726
DukHyung Kim <idealismboy@gmail.com>
2021-11-04 20:55:03 +0900

    WIP: collection-functions

---
+++ gobyexample/collection-functions/funcs.go
@@ -0,0 +1,25 @@
+package main
+
+func Index(vs []string, t string) int {
+
+}
+
+func Include(vs []string, t string) bool {
+
+}
+
+func Any(vs []string, f func(string) bool) bool {
+
+}
+
+func All(vs []string, f func(string) bool) bool {
+
+}
+
+func Filter(vs []string, f func(string) bool) []string {
+
+}
+
+func Map(vs []string, f func(string) string) []string {
+
+}
