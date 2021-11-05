0ccc9d396650226035a517429c82c5fb42795dc6
DukHyung Kim <idealismboy@gmail.com>
2021-11-06 00:03:52 +0900

    WIP: collection-functions3

--- gobyexample/collection-functions/main.go
+++ gobyexample/collection-functions/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
 	strs := []string{"peach", "apple", "pear", "plum"}
+
+	fmt.Println(Index(strs, "pear"))
+	fmt.Println(Include(strs, "grape"))
+	fmt.Println(Any(strs, func(s string) bool {
+		return strings.HasPrefix(s, "p")
+	}))
+	fmt.Println(All(strs, func(s string) bool {
+		return strings.HasPrefix(s, "p")
+	}))
+	fmt.Println(Filter(strs, func(s string) bool {
+		return strings.Contains(s, "e")
+	}))
+	fmt.Println(Map(strs, strings.ToUpper))
 }
