7207de4d161f5810c98f181a67670ca43fa87d54
DukHyung Kim <idealismboy@gmail.com>
2021-11-16 20:38:57 +0900

    add more print

--- bcrypt/main.go
+++ bcrypt/main.go
@@ -8,10 +8,13 @@ import (
 const originPassword = "my_secret_password"
 
 func main() {
+	log.Println("originPassword:", originPassword)
+
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(originPassword), bcrypt.DefaultCost)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	log.Println("hashedPassword:", string(hashedPassword))
 
 	if err := bcrypt.CompareHashAndPassword(hashedPassword, []byte(originPassword)); err != nil {
 		log.Fatalln(err)
