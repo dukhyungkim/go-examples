cc7f852df4bd80317d755c636c6adcc00ed92a08
DukHyung Kim <idealismboy@gmail.com>
2021-11-25 21:55:13 +0900

    rxgo from channel

---
+++ rxgo/from-channel/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/reactivex/rxgo/v2"
+)
+
+func main() {
+	ch := make(chan rxgo.Item)
+	go func() {
+		for i := 0; i < 9; i++ {
+			ch <- rxgo.Of(i)
+		}
+		close(ch)
+	}()
+	observable := rxgo.FromChannel(ch)
+
+	// first observer
+	for item := range observable.Observe() {
+		fmt.Println(item.V)
+	}
+
+	// second observer
+	for item := range observable.Observe() {
+		fmt.Println(item.V)
+	}
+}
