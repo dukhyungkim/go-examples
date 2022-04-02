26a55babc88db322a799a99e44b124e3db27cff4
DukHyung Kim <idealismboy@gmail.com>
2022-04-02 23:27:04 +0900

    move to deployments

--- database/deploy/mariadb-user.sql
+++
@@ -1,4 +0,0 @@
-create database toy;
-CREATE USER 'kimdh'@'%' IDENTIFIED BY 'rlaejrgud';
-GRANT ALL PRIVILEGES ON toy.* TO 'kimdh'@'%';
-FLUSH PRIVILEGES;
--- database/deploy/posgresql-user.sql
+++
@@ -1,3 +0,0 @@
-create database toy;
-create user kimdh with encrypted password 'rlaejrgud';
-grant all privileges on database toy to kimdh;
