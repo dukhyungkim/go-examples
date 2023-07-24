7a867a2d2a7bee8bc40a00636863a75f14bbf112
DukHyung Kim <idealismboy@gmail.com>
2023-07-24 21:24:37 +0900

    wire tutorial

--- di/wire/main.go
+++ di/wire/main.go
@@ -1,9 +1,7 @@
 package main
 
 func main() {
-	message := NewMessage("Hi there!")
-	greeter := NewGreeter(message)
-	event := NewEvent(greeter)
+	event := InitializeEvent("Hi there!")
 
 	event.Start()
 }
